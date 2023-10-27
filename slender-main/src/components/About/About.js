e973e4df7ca053789feb47f4f5ac556b3a573924
Andrius <andrius2500@gmail.com>
2023-10-27 18:34:22 +0300

    started about component

--- slender-main/src/components/About/About.js
+++ slender-main/src/components/About/About.js
@@ -1,7 +1,54 @@
 import React from "react";
+import "./about.css";
 
 const About = () => {
-  return <div>Game information</div>;
+  return (
+    <div className="about--container">
+      <div className="about--game-info-container">
+        <h1>Information about the game</h1>
+        <div>
+          <h2>Description</h2>
+          <p>
+            Step into the mysterious world of professional development with
+            "Portfolio Pursuit: The Slender Career." This unique and thrilling
+            game takes the spine-tingling concept of the Slender Man and melds
+            it with your journey to build a killer career portfolio. Will you be
+            able to navigate through the shadowy woods of your chosen field,
+            collect vital artifacts, and avoid the looming specter of career
+            stagnation?
+          </p>
+        </div>
+        <div>
+          <h2>Gameplay</h2>
+          <p>
+            In "Portfolio Pursuit," you take on the role of an ambitious
+            professional, desperately seeking to bolster your career portfolio.
+            As you navigate a daunting forest filled with obstacles and
+            challenges, you must collect key artifacts that represent your
+            accomplishments, skills, and experiences. These artifacts are
+            scattered throughout the game world, each holding the power to
+            enhance your portfolio. But beware, the Slender Career lurks in the
+            shadows, representing the fears and uncertainties that often haunt
+            professional growth. As you progress, this elusive figure will
+            attempt to thwart your efforts and slow your progress, symbolizing
+            the challenges and setbacks you may encounter in your real-life
+            career journey.
+          </p>
+        </div>
+        <div>
+          <h2>End notes</h2>
+          <p>
+            "Portfolio Pursuit: The Slender Career" is a game that challenges
+            you to confront your career-related fears head-on and develop a
+            portfolio that shines. Can you outwit the Slender Career and emerge
+            victorious, with a portfolio that impresses potential employers or
+            clients? It's time to find out and embark on this thrilling and
+            educational adventure.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
