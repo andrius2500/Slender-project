1ad8cf2661c1544f398740adfd963453a9bdf2cc
Andrius <andrius2500@gmail.com>
2023-11-17 20:18:18 +0200

    deleted commented code

--- src/components/StartWindow/StartWindow.js
+++ src/components/StartWindow/StartWindow.js
@@ -1,27 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-// import forestSound from "../assets/sound_effects/test.mp3";
 import "./start-window.css";
 
 const StartWindow = () => {
-  // const audio = new Audio(forestSound);
-
-  // useEffect(() => {
-  //   play();
-
-  //   return () => {
-  //     stop();
-  //   };
-  // });
-
-  // function play() {
-  //   audio.play();
-  // }
-
-  // function stop() {
-  //   audio.pause();
-  // }
-
   return (
     <div className="start-window--container">
       <ul className="start-window--ul">
